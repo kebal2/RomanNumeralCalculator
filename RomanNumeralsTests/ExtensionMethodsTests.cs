241417ba40af34de28b17ace12a610bb564781fe
Kelemen Bálint <kebal84@gmail.com>
2021-12-06 15:15:42 +0100

    minor refactoring

--- RomanNumeralsTests/ExtensionMethodsTests.cs
+++ RomanNumeralsTests/ExtensionMethodsTests.cs
@@ -1,5 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
-using System.Collections.Specialized;
+﻿using System.Collections.Specialized;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using RomanNumerals;
 
 namespace RomanNumeralsTests
@@ -10,14 +10,14 @@ namespace RomanNumeralsTests
         [TestMethod]
         public void GivenValue_WhenCheckingIfDictionaryContainsIt_AssertThatItExists()
         {
-            OrderedDictionary dict = new OrderedDictionary() { { 'V', 5 }, { 'X', 10 }, { 'L', 50 } };
+            var dict = new OrderedDictionary { { 'V', 5 }, { 'X', 10 }, { 'L', 50 } };
             Assert.IsTrue(dict.ContainsValue(5));
         }
 
         [TestMethod]
         public void GivenValue_WhenCheckingIfDictionaryContainsIt_AssertThatItDoesNotExists()
         {
-            OrderedDictionary dict = new OrderedDictionary() { { 'V', 5 }, { 'X', 10 }, { 'L', 50 } };
+            var dict = new OrderedDictionary { { 'V', 5 }, { 'X', 10 }, { 'L', 50 } };
             Assert.IsFalse(dict.ContainsValue(52));
         }
     }
