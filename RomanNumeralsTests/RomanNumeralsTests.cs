92a003f4f3156130f6d873f67d8c9282c8f6bb98
Kelemen Bálint <kebal84@gmail.com>
2021-12-06 15:15:15 +0100

    test case Assert proper value order

--- RomanNumeralsTests/RomanNumeralsTests.cs
+++ RomanNumeralsTests/RomanNumeralsTests.cs
@@ -9,120 +9,120 @@ namespace RomanNumeralsTests
         [TestMethod]
         public void GivenInteger1_WhenConvertedToRomanNumeral_AssertThatItsI()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(1);
-            Assert.AreEqual(romanNumeral, "I");
+            var romanNumeral = 1.AsRomanNumeral();
+            Assert.AreEqual("I", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger5_WhenConvertedToRomanNumeral_AssertThatItsV()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(5);
-            Assert.AreEqual(romanNumeral, "V");
+            var romanNumeral = 5.AsRomanNumeral();
+            Assert.AreEqual("V", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger10_WhenConvertedToRomanNumeral_AssertThatItsX()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(10);
-            Assert.AreEqual(romanNumeral, "X");
+            var romanNumeral = 10.AsRomanNumeral();
+            Assert.AreEqual("X", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger50_WhenConvertedToRomanNumeral_AssertThatItsL()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(50);
-            Assert.AreEqual(romanNumeral, "L");
+            var romanNumeral = 50.AsRomanNumeral();
+            Assert.AreEqual("L", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger100_WhenConvertedToRomanNumeral_AssertThatItsC()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(100);
-            Assert.AreEqual(romanNumeral, "C");
+            var romanNumeral = 100.AsRomanNumeral();
+            Assert.AreEqual("C", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger500_WhenConvertedToRomanNumeral_AssertThatItsD()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(500);
-            Assert.AreEqual(romanNumeral, "D");
-        }        
+            var romanNumeral = 500.AsRomanNumeral();
+            Assert.AreEqual("D", romanNumeral);
+        }
 
         [TestMethod]
         public void GivenInteger1000_WhenConvertedToRomanNumeral_AssertThatItsM()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(1000);
-            Assert.AreEqual(romanNumeral, "M");
+            var romanNumeral = 1000.AsRomanNumeral();
+            Assert.AreEqual("M", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger900_WhenConvertedToRomanNumeral_AssertThatItsCM()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(900);
-            Assert.AreEqual(romanNumeral, "CM");
+            var romanNumeral = 900.AsRomanNumeral();
+            Assert.AreEqual("CM", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger90_WhenConvertedToRomanNumeral_AssertThatItsXC()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(90);
-            Assert.AreEqual(romanNumeral, "XC");
+            var romanNumeral = 90.AsRomanNumeral();
+            Assert.AreEqual("XC", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger9_WhenConvertedToRomanNumeral_AssertThatItsIX()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(9);
-            Assert.AreEqual(romanNumeral, "IX");
+            var romanNumeral = 9.AsRomanNumeral();
+            Assert.AreEqual("IX", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger909_WhenConvertedToRomanNumeral_AssertThatItsCMIX()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(909);
-            Assert.AreEqual(romanNumeral, "CMIX");
+            var romanNumeral = 909.AsRomanNumeral();
+            Assert.AreEqual("CMIX", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger999_WhenConvertedToRomanNumeral_AssertThatItsCMXCIX()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(999);
-            Assert.AreEqual(romanNumeral, "CMXCIX");
+            var romanNumeral = 999.AsRomanNumeral();
+            Assert.AreEqual("CMXCIX", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger49_WhenConvertedToRomanNumeral_AssertThatItsXLIX()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(49);
-            Assert.AreEqual(romanNumeral, "XLIX");
+            var romanNumeral = 49.AsRomanNumeral();
+            Assert.AreEqual("XLIX", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger400_WhenConvertedToRomanNumeral_AssertThatItsCD()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(400);
-            Assert.AreEqual(romanNumeral, "CD");
+            var romanNumeral = 400.AsRomanNumeral();
+            Assert.AreEqual("CD", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger40_WhenConvertedToRomanNumeral_AssertThatItsXL()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(40);
-            Assert.AreEqual(romanNumeral, "XL");
+            var romanNumeral = 40.AsRomanNumeral();
+            Assert.AreEqual("XL", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger4_WhenConvertedToRomanNumeral_AssertThatItsIV()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(4);
-            Assert.AreEqual(romanNumeral, "IV");
+            var romanNumeral = 4.AsRomanNumeral();
+            Assert.AreEqual("IV", romanNumeral);
         }
 
         [TestMethod]
         public void GivenInteger723_WhenConvertedToRomanNumeral_AssertThatItsDCCXXIII()
         {
-            string romanNumeral = RomanNumeralCalculator.CalculateRomanNumeral(723);
-            Assert.AreEqual(romanNumeral, "DCCXXIII");
+            var romanNumeral = 723.AsRomanNumeral();
+            Assert.AreEqual("DCCXXIII", romanNumeral);
         }
     }
 }
